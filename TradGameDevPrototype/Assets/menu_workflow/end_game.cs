5b2d0fa3b8800bf61a10cb6910bb0c52fb8bf751
eliaslumarda <u0986615@umail.utah.edu>
2018-11-13 00:15:52 -0700

    Menu Workflow Added

---
+++ TradGameDevPrototype/Assets/menu_workflow/end_game.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class end_game : MonoBehaviour {
+
+	public void doExitGame()
+    {
+        Application.Quit();
+    }
+}
---
+++ TradGameDevPrototype/Assets/menu_workflow/scene_changer.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class scene_changer : MonoBehaviour {
+    
+    public void ChangeScene(string sceneName)
+    {
+        Application.LoadLevel(sceneName);
+    }
+
+}
