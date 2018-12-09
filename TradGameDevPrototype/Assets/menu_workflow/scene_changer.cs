618042d29514ce2b72c660c6fcda9c48bd58398c
Scott Foden <srfoden@gmail.com>
2018-12-09 03:17:37 -0700

    Changes to finalversion

--- TradGameDevPrototype/Assets/menu_workflow/scene_changer.cs
+++ TradGameDevPrototype/Assets/menu_workflow/scene_changer.cs
@@ -1,12 +1,14 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class scene_changer : MonoBehaviour {
     
     public void ChangeScene(string sceneName)
     {
-        Application.LoadLevel(sceneName);
+ 
+        SceneManager.LoadScene(sceneName);
     }
 
 }
