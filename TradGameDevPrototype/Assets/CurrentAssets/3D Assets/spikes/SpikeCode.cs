45a04a573187bed836d94814f2c285eaec3af03f
Scott Foden <srfoden@gmail.com>
2018-12-12 19:20:33 -0700

    More Changes

--- TradGameDevPrototype/Assets/CurrentAssets/3D Assets/spikes/SpikeCode.cs
+++ TradGameDevPrototype/Assets/CurrentAssets/3D Assets/spikes/SpikeCode.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpikeCode : MonoBehaviour {
 
     public float speed;
+    public float inclinedAngle;
+    public float zMax, zMin;
 
     float i = 0;
 
@@ -16,15 +18,16 @@ public class SpikeCode : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		
-        if (transform.position.z > 12.0f){
+        if (transform.position.z > zMax){
             speed *= -1;
         }
-        if (transform.position.z < -12.0f){
+        if (transform.position.z < zMin){
             speed *= -1;
         }
 
         Vector3 pos = transform.position;
         pos.z += speed * Time.deltaTime;
+        pos.y -= (speed * inclinedAngle) * Time.deltaTime;
 
         transform.position = pos;
 
@@ -33,7 +36,7 @@ public class SpikeCode : MonoBehaviour {
         transform.rotation = Quaternion.Slerp(transform.rotation, rot, Time.deltaTime * 20f);
 
         i++;
-        //transform.rotation = rot;
+
 
     }
 }
